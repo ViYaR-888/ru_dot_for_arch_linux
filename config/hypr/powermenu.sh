99f7af1030ca32c10ec342d5913e2064da00a2f6
ViYaR-888 <night1313131313@gmail.com>
2026-05-28 10:51:46 +0300

    Update powermenu.sh

--- config/hypr/powermenu.sh
+++ config/hypr/powermenu.sh
@@ -4,7 +4,6 @@ if killall rofi; then
     exit 0
 fi
 
-# Передаём только чистый текст без лишнего мусора
 chosen=$(echo -e "Suspend\nReboot\nShutdown" | rofi -dmenu -theme power -no-custom -disable-history)
 
 case "$chosen" in
